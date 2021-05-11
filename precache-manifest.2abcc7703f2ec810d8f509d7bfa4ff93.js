460fab5a4185f18c6a76419717448e812e81886d
MarcusHellkvist <hellqvistmarcus@gmail.com>
2021-05-11 22:34:44 +0200

    testlol

---
+++ precache-manifest.2abcc7703f2ec810d8f509d7bfa4ff93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88629b74ed63e6a6da3",
+    "url": "/css/app.6c2496cf.css"
+  },
+  {
+    "revision": "ae8519a3ba25d7c5dbd8",
+    "url": "/css/chunk-vendors.ff17b4ce.css"
+  },
+  {
+    "revision": "86ed267256f47e001bebd66def2ca853",
+    "url": "/img/crypto-logo.86ed2672.svg"
+  },
+  {
+    "revision": "c12c60b6bccc3af207bc105049dc01b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88629b74ed63e6a6da3",
+    "url": "/js/app.92961198.js"
+  },
+  {
+    "revision": "ae8519a3ba25d7c5dbd8",
+    "url": "/js/chunk-vendors.44458cda.js"
+  },
+  {
+    "revision": "bb0376088831d3d7e874122c12f6ed41",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
